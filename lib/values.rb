eb2d7823b5c4a5dcf06efdddeac7ef7825ffd236
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-06 03:25:04 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 #This is the groceries hash we'll be passing in to the get_the_values method
 # groceries = {
@@ -9,5 +9,14 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+groceries.values.flatten
 end
+
+ 
