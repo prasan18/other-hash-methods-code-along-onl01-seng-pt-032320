762a6d6abeb2523a54e395fbfa623cbbec054588
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-06 02:39:03 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,10 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
 end
