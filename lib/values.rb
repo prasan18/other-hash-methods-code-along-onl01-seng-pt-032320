7d71523a0f2d5d6ec3931c4e257d041f8bb6a207
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-06 03:23:12 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,13 +9,14 @@
 # }
 
 def get_the_values(groceries)
-  groceries.values.flatten
+
   groceries = {
   dairy: ["milk", "yogurt", "cheese"],
   vegetable: ["carrots", "broccoli", "cucumbers"],
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
 }
+groceries.values
 end
 
  
